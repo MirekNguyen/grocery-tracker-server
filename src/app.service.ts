4e0294ad882555f96632361921801346896e132c
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-10-20 14:48:11 +0200

    feat(app): integrate OpenAI for bedtime story generation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  public openApi: OpenAI;
+  private constructor() {
+    this.openApi = new OpenAI();
+  }
+  async getHello(): Promise<string> {
+    const response = await this.openApi.responses.create({
+      model: 'gpt-5-nano',
+      input: 'Write a one-sentence bedtime story about a unicorn.'
+    })
+    return response.output_text;
   }
 }
